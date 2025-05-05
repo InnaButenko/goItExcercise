4129809ff3f41405850eafde596ae75fe3fa5026
Inna Butenko <inna.butenko@students.bfh.ch>
2025-05-05 23:14:28 +0200

    Commit 05/05/2025 Added excercise Classes: UberShop, CheapStocks,ArlanHelper

---
+++ loops/loops/src/main/java/CheapStocks.java
@@ -0,0 +1,18 @@
+public class CheapStocks {
+    public String getCheapStocks(String[] stocks) {
+        StringBuilder stocksBuilder = new StringBuilder();
+        for (int i = 0; i < stocks.length; i++) {
+            String[] parts = stocks[i].split(" ");
+            String name = parts[0];
+            int price = Integer.parseInt(parts[1]);
+
+            if(price<200){
+                if (stocksBuilder.length()>0){
+                    stocksBuilder.append(" ");
+                }
+                stocksBuilder.append(name);
+            }
+        }
+        return stocksBuilder.toString();
+    }
+}
