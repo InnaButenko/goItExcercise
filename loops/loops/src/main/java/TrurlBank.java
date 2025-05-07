312974ecd1d24045739a07f5f4cf6108e7d80aa4
Inna Butenko <inna.butenko@students.bfh.ch>
2025-05-07 02:04:04 +0200

    Commit 02 06/05/2025 Added excercise Classes: TrurlBank - added excercise;

--- loops/loops/src/main/java/TrurlBank.java
+++ loops/loops/src/main/java/TrurlBank.java
@@ -1,4 +1,23 @@
 public class TrurlBank {
+    public int sumQuads(int n){
+        int sum = 0;
+        int i = 1;
+        while (i <= n){
+            sum += i * i;
+            i++;
+        }
+        return sum;
+    }
+
+    public int countSumOfDigits(int number){
+        int sum = 0;
+        while (number > 0){
+            sum += number % 10;
+            number = number / 10;
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         TrurlBank bank = new TrurlBank();
 
