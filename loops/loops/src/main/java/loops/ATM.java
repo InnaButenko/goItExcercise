1627634d7e404805e08e97867106de620d9bb16b
Inna Butenko <inna.butenko@students.bfh.ch>
2025-05-08 20:26:40 +0200

    Commit 01 08/05/2025

--- loops/loops/src/main/java/loops/ATM.java
+++ loops/loops/src/main/java/loops/ATM.java
@@ -1,18 +1,20 @@
 package loops;
 
 public class ATM {
-    public int countBanknotes(int sum){
+    public int countBanknotes(int sum) {
         int banknotes = 0;
-        for(int i = sum; i > 0; i--){
-            switch(sum% x) {
-                case x = 500:
-                    action1;
-                    break;
-                case value2:
-                    action2;
-                    break;
-            }
+//        for(int i = sum; i > 0; i--){
+//            switch(sum% x) {
+//                case x = 500:
+//                    action1;
+//                    break;
+//                case value2:
+//                    action2;
+//                    break;
+//            }
+//
+// }
 
     }
-
+    return;
 }
