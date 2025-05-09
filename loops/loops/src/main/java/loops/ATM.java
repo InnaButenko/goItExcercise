6f5c4f219282011c8a6253ab677b1d9e12ca0735
Inna Butenko <inna.butenko@students.bfh.ch>
2025-05-09 18:59:49 +0200

    Commit 01 09/05/2025 Class ATM - added excersices;

--- loops/loops/src/main/java/loops/ATM.java
+++ loops/loops/src/main/java/loops/ATM.java
@@ -3,18 +3,36 @@ package loops;
 public class ATM {
     public int countBanknotes(int sum) {
         int banknotes = 0;
-//        for(int i = sum; i > 0; i--){
-//            switch(sum% x) {
-//                case x = 500:
-//                    action1;
-//                    break;
-//                case value2:
-//                    action2;
-//                    break;
-//            }
-//
-// }
+        int[] nominals ={500, 200, 100, 50, 20, 10, 5, 2, 1};
+        for (int i = 0; i < nominals.length; i++) {
+           int count = sum / nominals[i];
+           banknotes += count;
+           sum = sum % nominals[i];
+        }
+        return banknotes;
+    }
+
+    public int countBanknotesWhile(int sum) {
+        int banknotes = 0;
+        int[] nominals = {500, 200, 100, 50, 20, 10, 5, 2, 1};
+        int i = 0;
+        while (sum > 0 && i < nominals.length) {
+            int count = sum / nominals[i];
+            if (count > 0) {
+                banknotes += count;
+                sum = sum % nominals[i];
+            }
+            i++;
+        }
+        return banknotes;
+    }
+
+
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+        int total = atm.countBanknotes(765);
+        int total2 = atm.countBanknotesWhile(765);
+        System.out.println("Total banknotes " + total);
 
     }
-    return;
 }
