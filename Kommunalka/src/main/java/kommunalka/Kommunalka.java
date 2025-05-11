e1c2501679bf567929d263866a701d839faafba4
Inna Butenko <inna.butenko@students.bfh.ch>
2025-05-11 13:55:48 +0200

    04 commit 11/05/2025 Changed Logic in Water, Electricity, Gas; Removed output in them, moved to Kommunalka class; Output structured;

--- Kommunalka/src/main/java/kommunalka/Kommunalka.java
+++ Kommunalka/src/main/java/kommunalka/Kommunalka.java
@@ -1,9 +1,18 @@
 package kommunalka;
 
 public class Kommunalka {
-    public Kommunalka(){}
+
     public static void main(String[] args) {
-        Kommunalka myKommunalka = new Kommunalka();
+        double coldAmount = MeterReadings.calculateMonthReadings(Month.JANUARY,568.0, 569.0);
+        double hotAmount = MeterReadings.calculateMonthReadings(Month.JANUARY,377.0, 380.0);
+        Water waterJanuary = new Water(Month.JANUARY, coldAmount, hotAmount);
+        //Water February = new Water(Month.FEBRUARY, coldAmount, hotAmount);
+        double dayElectricity = MeterReadings.calculateMonthReadings(Month.JANUARY,568.0, 569.0);
+        double nightElectricity = MeterReadings.calculateMonthReadings(Month.JANUARY,377.0, 380.0);
+        Electricity electricityJanuary = new Electricity(Month.JANUARY, dayElectricity, nightElectricity);
+        double gasAmount = MeterReadings.calculateMonthReadings(Month.JANUARY,568.0, 569.0);
+        Gas gasJanuary = new Gas(Month.JANUARY, gasAmount);
+        System.out.println("Media used in January:" + "\n Water: " + waterJanuary.getTotalWaterPrice() + "\n Electricity: " + electricityJanuary.getTotalElectricityPrice() + "\n Electricity: " + gasJanuary.getGasPrice());
 
     }
 }
--- Kommunalka/src/main/java/kommunalka/Water.java
+++ Kommunalka/src/main/java/kommunalka/Water.java
@@ -4,30 +4,34 @@ public class Water {
     private Month month;
     private double coldAmount;
     private double hotAmount;
-//    private double amount;
     private static final double coldfee = 2.0d;
     private static final double hotfee = 2.0d;
+    private double totalWaterPrice;
 
 
     public Water(Month month, double coldAmount, double hotAmount) {
         this.coldAmount = coldAmount;
         this.hotAmount = hotAmount;
+        //this.totalWaterPrice = coldAmount + hotAmount;
         double coldPrice = this.coldAmount * coldfee;
         double hotPrice = this.hotAmount * hotfee;
-        double totalPrice = coldPrice + hotPrice;
+        this.totalWaterPrice = coldAmount + hotAmount;
+        //double totalWaterPrice = coldPrice + hotPrice;
 
-        System.out.println("In " + month + " water costed:");
-        System.out.println("Cold Water: " + coldPrice);
-        System.out.println("Hot Water" + hotPrice);
-        System.out.println("Total Water" + totalPrice);
+//        System.out.println("In " + month + " water costed:");
+//        System.out.println("Cold Water: " + coldPrice);
+//        System.out.println("Hot Water" + hotPrice);
+//        System.out.println("Total Water" + totalWaterPrice);
     }
 
-
-    public static void main(String[] args) {
-        double coldAmount = MeterReadings.calculateMonthReadings(Month.JANUARY,568.0, 569.0);
-        double hotAmount = MeterReadings.calculateMonthReadings(Month.JANUARY,377.0, 380.0);
-        Water January = new Water(Month.JANUARY, coldAmount, hotAmount);
-        Water February = new Water(Month.FEBRUARY, coldAmount, hotAmount);
-
+    public double getTotalWaterPrice() {
+        return totalWaterPrice;
     }
+    //    public static void main(String[] args) {
+//        double coldAmount = MeterReadings.calculateMonthReadings(Month.JANUARY,568.0, 569.0);
+//        double hotAmount = MeterReadings.calculateMonthReadings(Month.JANUARY,377.0, 380.0);
+//        Water January = new Water(Month.JANUARY, coldAmount, hotAmount);
+//        Water February = new Water(Month.FEBRUARY, coldAmount, hotAmount);
+//
+//    }
 }
